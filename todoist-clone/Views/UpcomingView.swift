6388f48316eece9dcb27e892047e9bf2792bd5cd
Andrii Pashuta <andrii.pashuta@gmail.com>
2024-10-30 20:20:33 +0100

    Added basic UI

---
+++ todoist-clone/Views/UpcomingView.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingView.swift
+//  todoist-clone
+//
+//  Created by Andrii Pashuta on 30.10.2024.
+//
+
+import SwiftUI
+
+struct UpcomingView: View {
+    var body: some View {
+        Text("tbd")
+    }
+}
+
+#Preview {
+    UpcomingView()
+}
